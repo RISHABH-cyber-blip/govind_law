33ef5afe38a32a037cf82253a3e4413052a5ff41
Rishabh <rm1994269@gmail.com>
2026-08-04 20:19:34 +0530

    jdss
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  basePath: "/govind_law",
+  assetPrefix: "/govind_law/",
+
   images: {
+    unoptimized: true,
+
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
