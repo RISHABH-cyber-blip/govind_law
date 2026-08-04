8b26d4531496deb31168f97d2a3fb340cff5ba33
Rishabh <rm1994269@gmail.com>
2026-08-04 20:33:46 +0530

    change in nesxt
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-
-  basePath: "/govind_law",
-  assetPrefix: "/govind_law/",
-
   images: {
-    unoptimized: true,
-
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
   },
 };
 
