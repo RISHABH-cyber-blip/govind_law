1833a6e45e9d6bbf1200ee0350dd4636328feb11
Rishabh <rm1994269@gmail.com>
2026-08-11 23:23:06 +0530

    ggd
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://govind-law.vercel.app/";
+  const baseUrl = "https://govind-law.vercel.app";
   const now = new Date();
 
   return [
